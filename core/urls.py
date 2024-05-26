637596b98d528a0a502f786dbc6cefaeb1c4d914
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2024-05-26 15:08:24 -0300

    mudancas

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from autenticacao.views import HomeView, SigupView
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('estoque/', include('estoque.urls')),
     path('admin/', admin.site.urls),
-    path('', include('estoque.urls')),
+    path('',include('autenticacao.urls')),
+    
+   
 ]
