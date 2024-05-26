3afd1c1de4101a5f46483e4a2d8922fa4170c9b1
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2024-05-26 14:50:25 -0300

    Inclusao do admin

--- estoque/urls.py
+++ estoque/urls.py
@@ -4,14 +4,30 @@
 
 # urls.py
 
-from django.urls import path
+
+from django.contrib import admin
+from django.urls import path, include
+
+from autenticacao.views import HomeView, SigupView
+from django.shortcuts import redirect
+
 from .views import AlimentoListView, AlimentoCreateView, AlimentoUpdateView, AlimentoDeleteView
 from . import views
 
 urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    path('',include('autenticacao.urls')),
+    path('estoque/', include('estoque.urls')),
+    
     path('', AlimentoListView.as_view(), name='alimento_list'),
     path('home/', views.home, name='home'),
     path('create/', AlimentoCreateView.as_view(), name='alimento_create'),
     path('update/<int:pk>/', AlimentoUpdateView.as_view(), name='alimento_update'),
     path('delete/<int:pk>/', AlimentoDeleteView.as_view(), name='alimento_delete'),
 ]
+
+
+
+
+
